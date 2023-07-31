9edf7c512a66f6353ed71ea2cc144c28ec51e70d
“Natacha” <aina2natacha@gmail.com>
2023-07-31 16:06:58 +0300

    add cv

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MyButton from './myButton'
 import '../css/AboutMe.css'
+import resume from '../resume/CV-NATA.pdf'
 
 
 export default function AboutMe() {
@@ -14,7 +15,7 @@ export default function AboutMe() {
                 <div className='aboutme m-auto w-full lg:w-3/5 p-6 rounded-2xl text-center'>
                     <h1>Bonjour, je suis Natacha</h1>
                     <p className='mb-4'> Développeuse passionnée dans le domaine du web. Après une formation dans le Web, j'ai acquis de nombreuses compétences et connaissances grâce à des projets réalisés dans différents langages de programmation. J'ai quelques projets intéressants <a href='#' style={{ color: '#862B0D' }}>à voir</a>.  </p>
-                    <MyButton titre="TELECHARGER MON CV" />
+                    <a className="btn btn-outline btn-warning hover:bg-tertiary hover:text-primary text-slate-300" href={resume} download="Mon CV">TELECHARGER MON CV</a>
                 </div>
                 <div className='aboutme p-6 rounded-2xl flex justify-between text-center m-auto w-full flex-col space-y-4 sm:flex-row sm:space-y-0 lg:w-3/5 '>
                     <div className='w-full'>
