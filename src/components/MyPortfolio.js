7b89dfa6cf7247c93786487c3b599979e5abab7b
“Natacha” <aina2natacha@gmail.com>
2023-09-07 09:18:59 +0300

    ekiraro

--- src/components/MyPortfolio.js
+++ src/components/MyPortfolio.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import '../css/MyPortfolio.css'
 import Projet from './Projet'
-import ravimaintso from '../assets/ravimaintso.jpg'
 import blog from '../assets/blog.jpg'
 import todo from '../assets/todo.png'
 import pizza from '../assets/pizza.png'
+import ekiraro from '../assets/ekiraro.png'
 
 
 export default function MyPortfolio() {
@@ -16,10 +16,10 @@ export default function MyPortfolio() {
             </div>
             <div className='flex m-auto justify-center'>
                 <div className='flex space-x-0 space-y-6 flex-col px-7 w-full sm:space-y-0 sm:space-x-4 sm:flex-row lg:w-3/5 lx:px-0 text-center text-xl '>
-                    <Projet image={ravimaintso} text='HUILE ESSENTIELLE' url="https://ainanatacha22.github.io/ravitsara/"/>
+                    <Projet image={pizza} text='PIZZA' url="https://ainanatacha22.github.io/ventepizza/" />
+                    <Projet image={ekiraro} text='Ekiraro' url="https://64f9640022182a359d36fb7f--e-kiraro.netlify.app/" />
                     <Projet image={blog} text='BLOG' url="https://myblognat.netlify.app/"/>
                     <Projet image={todo} text='TODO LIST' url=" https://todolist-nat.netlify.app/"/>
-                    <Projet image={pizza} text='PIZZA' url="https://ainanatacha22.github.io/ventepizza/" />
                 </div>
             </div>
         </div>
